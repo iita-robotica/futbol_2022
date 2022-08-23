7fbdaaaa45f54a3f2221b0c0f7982c378e642d08
IgnacioSosa <nachososa2007@gmail.com>
2022-08-23 20:13:52 -0300

    agrego mi equipo Grupo 1

---
+++ Competencia_Local/Grupo1_Maximo_Ignacio/roles.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from math_utils.point import Point
+from math_utils.angle import degrees
+
+class BallFollower:
+    def applyOn(self, robot, snapshot):
+        if snapshot.ball != None:
+            robot.moveToBall()
+        else:
+            robot.moveToPoint(Point.ORIGIN)
+            
+class Goalkeeper:
+    def applyOn(self, robot, snapshot):
+        if snapshot.color == "B":
+            equipo = 1
+        else:
+            equipo = -1
+
+        if snapshot.ball != None:
+            ball = snapshot.ball.position
+        else:
+            ball = Point.ORIGIN
+
+        target = Point(ball.x, equipo * 0.55)
+        if robot.getPosition().dist(target) < 0.01:
+            robot.lookAtAngle(degrees(90))
+        else:
+            robot.moveToPoint(target)
+
+# Tercer rol 
+class Defender:
+    def applyOn(self, robot, snapshot):
+        if snapshot.color == "B":
+            equipo = 1
+        else:
+            equipo = -1
+
+        if snapshot.ball != None:
+            ball = snapshot.ball.position
+        else:
+            ball = Point.ORIGIN
+
+        target = Point(ball.x, equipo * 0.25)
+
+        if robot.getPosition().dist(target) < 0.01:
+            robot.lookAtAngle(degrees(90))
+        else:
+            robot.moveToPoint(target)
+
+            
