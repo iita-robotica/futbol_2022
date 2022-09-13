3ab0f19f4191caee71e1f6ad17b21aed2f8e3f9e
IgnacioSosa <nachososa2007@gmail.com>
2022-09-13 19:35:05 -0300

    borro esta carpeta

--- IITA/nodeberiaexistir/IITA/math_utils/utils.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Restringe el valor numérico "n" entre "smallest" y "largest"
-def clamp(n, smallest, largest): 
-    return max(smallest, min(n, largest))
--- IITA/nodeberiaexistir/IITA/rsproxy/server.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-Server = None
-try:
-    from rsproxy.webots_server import WebotsServer
-    Server = WebotsServer
-except:
-    from rsproxy.udp_server import UDPServer
-    Server = UDPServer
-
