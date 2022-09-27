9230bce56e78f3ca5191dd039b22515795417d8a
URGerardo <uribururomerogerardocdm@gmail.com>
2022-09-27 20:44:30 -0300

    Agrego en roles.py funciones de defensa

--- Competencia_Local/MagnetIITA_Gerardo/roles.py
+++ Competencia_Local/MagnetIITA_Gerardo/roles.py
@@ -3,6 +3,12 @@ import random
 from math_utils.point import Point
 from math_utils.angle import degrees
 
+def signoPosPelota(pelotaX):
+    if pelotaX > 0:
+        return 1
+    elif pelotaX < 0:
+        return -1
+
 class BallFollower:
     def applyOn(self, robot, snapshot):
         if snapshot.color == "B":
@@ -32,7 +38,12 @@ class Goalkeeper:
         if robot.getPosition().dist(target) < equipo * 0.01: #Si el balón se encuentra dentro del rango del área no continuar hasta la colisión con pared - si es mayor al absoluto de 0.35 
             robot.lookAtAngle(degrees(90))
         else:
-            robot.moveToPoint(target)
+            if abs(ball.x) > 0.5:
+                signo = signoPosPelota(ball.x)
+                targetLateral = Point(0.35*signo, equipo * 0.55)
+                robot.moveToPoint(targetLateral)
+            else:
+                robot.moveToPoint(target)   
 
 class Defender:
     def applyOn(self, robot, snapshot):
